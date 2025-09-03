701d0123d26760d9a2ca3754c52de83478bdcf96
Shadowgorat23 <santisanchez21456@gmail.com>
2025-09-02 22:40:10 -0500

    10:39 PM, Creación y adición del primer caso de ML supervisado

--- A3_CasosUsoMLSupervisado/app.py
+++ A3_CasosUsoMLSupervisado/app.py
@@ -10,7 +10,7 @@ def index():
 @app.route('/PrimerCaso')
 def PrimerC():
      myname = "Flask"
-     return render_template('PrimerC.html',name=myname)
+     return render_template('PrimerCaso.html',name=myname)
 
 @app.route('/SegundoCaso')
 def SegundoC():
@@ -25,7 +25,7 @@ def TercerC():
 @app.route('/CuartoCaso')
 def CuartoC():
      myname = "Flask"
-     return render_template('CuartoC.html',name=myname)
+     return render_template('CuartoCaso.html',name=myname)
 
 if __name__ == '__main__':
      app.run(debug=True)
