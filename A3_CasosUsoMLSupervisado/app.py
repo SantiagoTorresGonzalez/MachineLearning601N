30a87e1297a464d6f5711f9d8f92b12f7215dc05
SantiagoTorresGonzalez <setorres@ucundinamarca.edu.co>
2025-09-02 22:09:06 -0500

    10:05 pm, Se modifico el codigo del index.html con nuevos titulos,se agregaron botones desplegables con las diferentes direcciones para las investigaciones de cada integrante y se agrego un nuevo archivo en la carpeta templates llamado 'SegundoC' el cual contiene el codigo de la paguina donde esta descrito una de los casos de exito de machine learning supervisado

--- A3_CasosUsoMLSupervisado/app.py
+++ A3_CasosUsoMLSupervisado/app.py
@@ -2,16 +2,30 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def home ():
-     myname = None
-     myname = "Flask" 
-     return f"Hello, {myname}!"
-
-@app.route('/index')
+@app.route('/')
 def index():
      myname = "Flask"
      return render_template('index.html',name=myname)
+
+@app.route('/PrimerCaso')
+def PrimerC():
+     myname = "Flask"
+     return render_template('PrimerC.html',name=myname)
+
+@app.route('/SegundoCaso')
+def SegundoC():
+     myname = "Flask"
+     return render_template('SegundoC.html',name=myname)
+
+@app.route('/TercerCaso')
+def TercerC():
+     myname = "Flask"
+     return render_template('TercerC.html',name=myname)
+
+@app.route('/CuartoCaso')
+def CuartoC():
+     myname = "Flask"
+     return render_template('CuartoC.html',name=myname)
 
 if __name__ == '__main__':
      app.run(debug=True)
